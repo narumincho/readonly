80cab72766517d6adad32b27358eb5edf33f1dd9
narumincho <16481886+narumincho@users.noreply.github.com>
2024-07-13 10:03:25 +0900

    add ReadonlyURL, ReadonlyURLSearchParams

---
+++ test.ts
@@ -0,0 +1,14 @@
+import { ReadonlyURL, ReadonlyURLSearchParams } from "./mod.ts";
+import { assertEquals } from "jsr:@std/assert";
+
+Deno.test("url", () => {
+  const url: ReadonlyURL = new URL("https://example.com");
+  assertEquals(url.href, "https://example.com/");
+});
+
+Deno.test("searchParams", () => {
+  const url: ReadonlyURLSearchParams = new URLSearchParams(
+    "a=3&b=4&c=5",
+  );
+  assertEquals(url.size, 3);
+});
