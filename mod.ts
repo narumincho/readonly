0ec6a08da3db73e2e51ce91b5665b1c83c918baa
narumincho <16481886+narumincho@users.noreply.github.com>
2024-07-14 15:00:13 +0900

    add ReadonlyInt16Array

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 export * from "./URL.ts";
 export * from "./TypedArray/Uint8Array.ts";
 export * from "./TypedArray/Uint8ClampedArray.ts";
+export * from "./TypedArray/Int16Array.ts";
--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
 import type {
+  ReadonlyInt16Array,
   ReadonlyUint8Array,
   ReadonlyUint8ClampedArray,
   ReadonlyURL,
@@ -6,27 +7,34 @@ import type {
 } from "./mod.ts";
 import { assertEquals } from "jsr:@std/assert";
 
-Deno.test("ReadonlyURL", () => {
+Deno.test("URL", () => {
   const value: ReadonlyURL = new URL("https://example.com");
   assertEquals(value.href, "https://example.com/");
 });
 
-Deno.test("ReadonlyURLSearchParams", () => {
+Deno.test("URLSearchParams", () => {
   const value: ReadonlyURLSearchParams = new URLSearchParams(
     "a=3&b=4&c=5",
   );
   assertEquals(value.size, 3);
 });
 
-Deno.test("ReadonlyUint8Array", () => {
+Deno.test("Uint8Array", () => {
   const value: ReadonlyUint8Array = new Uint8Array(
     [4, 5, 6],
   );
   assertEquals(value[1], 5);
 });
 
-Deno.test("ReadonlyUint8ClampedArray", () => {
+Deno.test("Uint8ClampedArray", () => {
   const value: ReadonlyUint8ClampedArray = new Uint8ClampedArray(
+    [4, 5, 6],
+  );
+  assertEquals(value[1], 5);
+});
+
+Deno.test("Int16Array", () => {
+  const value: ReadonlyInt16Array = new Int16Array(
     [4, 5, 6],
   );
   assertEquals(value[1], 5);
