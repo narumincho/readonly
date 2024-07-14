a95f5f341619c2c1906faf13817c987d9c7077ad
narumincho <16481886+narumincho@users.noreply.github.com>
2024-07-14 14:03:38 +0900

    add ReadonlyUint8Array

--- test.ts
+++ test.ts
@@ -1,14 +1,25 @@
-import type { ReadonlyURL, ReadonlyURLSearchParams } from "./mod.ts";
+import type {
+  ReadonlyUint8Array,
+  ReadonlyURL,
+  ReadonlyURLSearchParams,
+} from "./mod.ts";
 import { assertEquals } from "jsr:@std/assert";
 
-Deno.test("url", () => {
+Deno.test("ReadonlyURL", () => {
   const url: ReadonlyURL = new URL("https://example.com");
   assertEquals(url.href, "https://example.com/");
 });
 
-Deno.test("searchParams", () => {
+Deno.test("ReadonlyURLSearchParams", () => {
   const url: ReadonlyURLSearchParams = new URLSearchParams(
     "a=3&b=4&c=5",
   );
   assertEquals(url.size, 3);
+});
+
+Deno.test("ReadonlyUint8Array", () => {
+  const uint8Array: ReadonlyUint8Array = new Uint8Array(
+    [4, 5, 6],
+  );
+  assertEquals(uint8Array[1], 5);
 });
